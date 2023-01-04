fc8e7a6f807516657f7960f8c8083bf99b997450
vhprestes <vhprestes@gmail.com>
2023-01-04 19:21:58 -0300

    fix typo

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,11 +3,11 @@ import Textbox from '../Components/Textbox';
 
 function About() {
   const text =
-    'Nascido em Paracambi, no interior do Rio de Janeiro, aonde resido atualmente. ' +
+    'Nascido em Paracambi, no interior do Rio de Janeiro, onde resido atualmente. ' +
     ' Sempre fui um entusiasta de tecnologia e programação, mas a princípio optei por seguir a carreira na Industria.' +
     ' Me formei em Manutenção Industrial na UTFPR em 2012, e entrei na UFF para cursar Engenharia de Produção em 2020,' +
-    ' atualmente estou com esse curso trancado no 5 período, pois optei pela transição de carreira.' +
-    ' Sou uma pessoa autodidata e disciplinada, tenho como hobbys a música e a músculação.';
+    ' atualmente estou com este trancado no 5 período, pois optei pela transição de carreira.' +
+    ' Sou uma pessoa autodidata e disciplinada, tenho como hobbies a música e a músculação.';
 
   const text2 = 'Possuo disponibilidade para trabalho hibrido ou presencial no Rio de Janeiro (Capital, baixada e sul fluminense) e para mudança caso seja necessário. '
   return (
