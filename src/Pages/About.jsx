5e5958be956e37ed4ed18897f253d4f26891cbc4
vhprestes <vhprestes@gmail.com>
2023-05-27 14:29:24 -0300

    feat: update about me

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,10 +6,10 @@ function About() {
     'Nascido em Paracambi, no interior do Rio de Janeiro, onde resido atualmente. ' +
     ' Sempre fui um entusiasta de tecnologia e programação, mas a princípio optei por seguir a carreira na Industria.' +
     ' Me formei em Manutenção Industrial na UTFPR em 2012, e entrei na UFF para cursar Engenharia de Produção em 2020,' +
-    ' atualmente estou com este trancado no 5 período, pois optei pela transição de carreira.' +
-    ' Sou uma pessoa autodidata e disciplinada, tenho como hobbies a música e a músculação.';
+    ' Contudo, ao optar pela transição de carreira, iniciei e conclui meus estudos na Trybe e atualmente sou acadêmico de Ciencia da Computação com conclusão prevista para 2026.' +
+    ' Sou uma pessoa autodidata e disciplinada, tenho como hobbies a música e a músculação. ';
 
-  const text2 = 'Possuo disponibilidade para trabalho hibrido ou presencial no Rio de Janeiro (Capital, baixada e sul fluminense) e para mudança caso seja necessário. '
+  const text2 = 'Possuo disponibilidade para trabalho remoto, hibrido ou presencial no Rio de Janeiro (Capital, baixada e sul fluminense) e para mudança caso seja necessário. '
   return (
     <div>
       <div className='aboutContainer'>
