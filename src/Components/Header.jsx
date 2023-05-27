7adc424c736305fc67a2215b80178c58b9490312
vhprestes <vhprestes@gmail.com>
2023-05-27 14:29:40 -0300

    feat: update dfisplay icons, removing buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,15 +7,9 @@ function Header({ handleComponentChange }) {
       <i className="fa-solid fa-house-user fa"  onClick={() => handleComponentChange('main')}>
         <Button className="BtnHeader" data-mdb-ripple-color="dark" text="main" />
       </i>
-      <i>
-        <Button className="BtnHeader btn btn-light btn-outline-red" onClick={() => handleComponentChange('main')} text="Home" />
-      </i>
 
       <a href="https://github.com/vhprestes/" target="_blank" className="fa-brands fa-github fa fa-inverse">
         <Button className="BtnHeader" data-mdb-ripple-color="dark" text="Github" />
-      </a>
-      <a href="https://github.com/vhprestes/" target="_blank">
-        <Button className="BtnHeader btn btn-light btn-outline-red" data-mdb-ripple-color="dark" text="Github" />
       </a>
 
 
@@ -23,23 +17,14 @@ function Header({ handleComponentChange }) {
       <i className="fa fa-briefcase fa-inverse"  onClick={() => handleComponentChange('projects')}>
         <Button className="BtnHeader" data-mdb-ripple-color="dark" text="Projetos" />
       </i>
-      <i>
-        <Button className="BtnHeader btn btn-light btn-outline-red" data-mdb-ripple-color="dark" text="Projetos" onClick={() => handleComponentChange('projects')} />
-      </i>
 
       <i className="fa-solid fa-phone fa fa-inverse "  onClick={() => handleComponentChange('contact')}>
         <Button className="BtnHeader" data-mdb-ripple-color="dark" text="Contato" onClick={() => handleComponentChange('contact')} />
-      </i>
-      <i>
-        <Button className="BtnHeader btn btn-light btn-outline-red" data-mdb-ripple-color="dark" text="Contato" onClick={() => handleComponentChange('contact')} />
       </i>
 
 
       <a href="https://linkedin.com/in/vhprestes/" target="_blank" className="fa-brands fa-linkedin fa fa-inverse ">
         <Button className="BtnHeader" data-mdb-ripple-color="dark" text="Linkedin" />
-      </a>
-      <a href="https://linkedin.com/in/vhprestes/" target="_blank">
-        <Button className="BtnHeader btn btn-light btn-outline-red" data-mdb-ripple-color="dark" text="Linkedin" />
       </a>
 
     </header>
