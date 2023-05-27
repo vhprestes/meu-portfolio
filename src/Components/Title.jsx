c343f63c84e6ee245559ab7f81a871ebfc5214ce
vhprestes <vhprestes@gmail.com>
2023-05-27 14:30:21 -0300

    feaT: updating title component

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -2,7 +2,10 @@ function Title() {
   return (
     <div className='title'>
       <h1>Olá! Eu sou o Victor Prestes!</h1>
-      <h2> Sou um desenvolvedor full stack formado na Trybe </h2>
+      <h2> Sou um desenvolvedor full stack formado na Trybe e acadêmico de Ciência da Computação</h2>
+      <br />
+      <br />
+
     </div>
   );
 }
